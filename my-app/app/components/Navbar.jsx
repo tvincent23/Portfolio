bf0376d1e3a8893a4ba04ab6c3f7fa2f37f701fc
Thomas Vincent <thomasvincent@live.co.uk>
2024-01-16 08:47:31 +0000

    navbar image centred

--- my-app/app/components/Navbar.jsx
+++ my-app/app/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 border-b border-[#33353F] bg-opacity-100 max-h-48">
       <div className="flex flex-wrap items-center justify-between lg:mr-20 lg:ml-5 px-4 py-4 text-white">
-        <div className="flex justify-center items-center pt-3 pl-5 max-w-12 max-h-12 border border-indigo-600">
+        <div className="flex justify-center items-center max-w-12 max-h-12 border border-indigo-600">
           <Image className="scale-75" src={Avatar} alt="avatar image" />
         </div>
         {/* <code className="font-mono font-bold">app/page.js</code> */}
