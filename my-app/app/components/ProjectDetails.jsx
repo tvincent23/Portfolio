13638fbe1fc2da184a8f33d62f40cf65c9a06cf0
Thomas Vincent <thomasvincent@live.co.uk>
2024-01-24 22:04:15 +0000

    border added to projects

--- my-app/app/components/ProjectDetails.jsx
+++ my-app/app/components/ProjectDetails.jsx
@@ -4,10 +4,10 @@ const ProjectDetails = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
-        className="h-52 md:h-72 rounded-t-xl relative group"
+        className="h-52 md:h-72 rounded-t-xl relative group border-t-2 border-l-2 border-r-2 border-white-600"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       ></div>
-      <div className="text-white">
+      <div className="text-white rounded-b-xl bg-[#181818] border-b-2 border-l-2 border-r-2 border-white-600">
         <h5>{title}</h5>
         <p>{description}</p>
       </div>
