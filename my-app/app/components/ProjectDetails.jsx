b15ef2f5b61e50bf959fd9698c7de8ac0ea7e785
Thomas Vincent <thomasvincent@live.co.uk>
2024-01-30 10:21:46 +0000

    github icon added as a link and rendered if link is available

--- my-app/app/components/ProjectDetails.jsx
+++ my-app/app/components/ProjectDetails.jsx
@@ -1,15 +1,69 @@
 import React from "react";
+import { EyeIcon } from "@heroicons/react/24/outline";
+import githubLogo from "../../public/images/github-mark-white.png";
+import Image from "next/image";
+import Link from "next/link";
 
 const ProjectDetails = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
+  console.log(gitUrl);
   return (
+    // <div className="mt-8">
+    //   <div
+    //     className="w-[312px] h-72 lg:w-[376px] rounded-t-xl relative group border-t-2 border-l-2 border-r-2 border-white-600"
+    //     style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+    //   ></div>
+    //   <div className=" text-white w-[312px] lg:w-[376px] md:h-[150px] rounded-b-xl bg-[#181818]  border-b-2 border-l-2 border-r-2 border-white-600 p-3">
+    //     <div>
+    //       <span className="text-blue-400 font-bold text-xl">
+    //         {title}
+    //         <Link href={previewUrl} target="_blank">
+    //           <EyeIcon className="h-7 w-7 " />
+    //         </Link>
+    //         {/* <image src={githubLogo} alt="GitHub Logo" className="scale-75" /> */}
+    //         {/* not showing github image!!!!!!!! */}
+    //       </span>
+    //     </div>
+    //     <p className="mt-4">{description}</p>
+    //   </div>
+    // </div>
+    // <div className="mt-8">
+    //   <div
     <div className="mt-8">
       <div
-        className="h-52 md:h-72 rounded-t-xl relative group border-t-2 border-l-2 border-r-2 border-white-600"
+        className="w-[312px] h-72 lg:w-[376px] rounded-t-xl relative group border-t-2 border-l-2 border-r-2 border-white-600"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       ></div>
-      <div className="text-white rounded-b-xl bg-[#181818] border-b-2 border-l-2 border-r-2 border-white-600 p-2">
-        <h5>{title}</h5>
-        <p>{description}</p>
+      <div className=" text-white w-[312px] lg:w-[376px] md:h-[150px] rounded-b-xl bg-[#181818]  border-b-2 border-l-2 border-r-2 border-white-600 p-3">
+        <div className="grid grid-cols-8">
+          <div className="col-span-6 text-blue-400 font-bold text-xl">
+            {title}
+          </div>
+          <div className="col-span-1">
+            {gitUrl.length > 1 ? (
+              <Link href={gitUrl} target="_blank">
+                <Image
+                  src={githubLogo}
+                  alt="GitHub Logo"
+                  className="w-[25px] h-[25px]"
+                />
+              </Link>
+            ) : null}
+
+            {/* <Link href={gitUrl} target="_blank">
+              <Image
+                src={githubLogo}
+                alt="GitHub Logo"
+                className="w-[25px] h-[25px]"
+              />
+            </Link> */}
+          </div>
+          <div className="col-span-1">
+            <Link href={previewUrl} target="_blank ">
+              <EyeIcon className="h-7 w-7 " />
+            </Link>
+          </div>
+        </div>
+        <p className="mt-4">{description}</p>
       </div>
     </div>
   );
--- my-app/app/components/Projects.jsx
+++ my-app/app/components/Projects.jsx
@@ -7,18 +7,29 @@ const Projects = () => {
     {
       id: 1,
       title: "Beyond Barriers",
-      description: "description...",
+      description:
+        "A disability community app designed to improve the process of discovering if a venue is suitable for specific needs.",
       image: "/images/img2bb.png",
       gitUrl: "/",
-      previewUrl: "/",
+      previewUrl: "https://beyond-barriers.vercel.app/",
     },
     {
       id: 2,
       title: "Portfolio",
-      description: "description...",
+      description:
+        "My portfolio site. Built using Next JS, JavaScript and TailwindCSS.",
       image: "/images/img3p.png",
       gitUrl: "/",
-      previewUrl: "/",
+      previewUrl: "https://thomas-vincent.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Noughts & Crosses",
+      description:
+        "Noughts & Crosses app, a single day hackathon in a team of 2. Our first effort using React.",
+      image: "/images/img4nac.png",
+      gitUrl: "https://github.com/SchoolOfCode/Noughts_And_Crosses_App",
+      previewUrl: "https://noughts-and-crosses-kappa.vercel.app/",
     },
   ];
 
@@ -27,13 +38,15 @@ const Projects = () => {
       <h2 className="mt-36 text-center text-white font-bold text-3xl 2xl:text-4xl">
         Projects
       </h2>
-      <div className="mx-auto grid grid-cols-1 md:grid-cols-2 sm:gap-6 md:gap-12 max-w-[312px] md:max-w-[800px]">
+      <div className="mx-auto grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 sm:gap-6 md:gap-12  max-w-[312px] md:max-w-[800px] 2xl:max-w-[1400px]">
         {projectsData.map((project) => (
           <ProjectDetails
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
+            previewUrl={project.previewUrl}
+            gitUrl={project.gitUrl}
           />
         ))}
       </div>
